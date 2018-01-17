1779e5e5d9c05f5abb6125750e7d25c04f60bc5f
Unknown <j.arnstadt@art-ifact.de>
2018-01-17 19:56:39 +0100

    added app icons

--- index.php
+++ index.php
@@ -3,8 +3,27 @@
 <html <?php language_attributes(); ?> class="no-js no-svg">
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no">
   <meta name="theme-color" content="#212121" />
+  <link rel="apple-touch-icon" sizes="57x57" href="/appicons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/appicons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/appicons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/appicons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/appicons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/appicons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/appicons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/appicons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/appicons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/appicons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/appicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/appicons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/appicons/favicon-16x16.png">
+    <link rel="manifest" href="manifest.json">
+    <meta name="msapplication-TileColor" content="#212121">
+    <meta name="msapplication-TileImage" content="/appicons/ms-icon-144x144.png">
+
+  <meta name="mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-capable" content="yes">
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <?php wp_head(); ?>
   <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons">
